07689d1e4fceba00f7b7887628e4c3bdedde9f56
Luis Villa <luisji2312ia@gmail.com>
2022-03-17 23:38:08 -0600

    v34 Categorias en material de apoyo (botones)

--- build/eleccionPC.js
+++ build/eleccionPC.js
@@ -7,13 +7,36 @@ const btnHijoPC = document.querySelector('.eleccionPC_hijo');
 const contenedorPadrePC = document.querySelector('.tutores');
 const contenedorHijoPC = document.querySelector('.infantes');
 
+const btnEnlaces = document.querySelector('.enlaces');
+const btnVideos = document.querySelector('.videos');
+const btnDocumentos = document.querySelector('.documentos');
+
+
+
+
 function botones() {
     btnPadrePC.onclick = function() {
         contenedorPadrePC.classList.add('visible');
+        btnPadrePC.classList.add('activo');
         contenedorHijoPC.classList.remove('visible');
+        btnHijoPC.classList.remove('activo');
     }
     btnHijoPC.onclick = function() {
         contenedorPadrePC.classList.remove('visible');
         contenedorHijoPC.classList.add('visible');
+        btnPadrePC.classList.remove('activo');
+        btnHijoPC.classList.add('activo');
+    }
+    btnEnlaces.onclick = function() {
+
+        console.log('enlaces')
+    }
+    btnVideos.onclick = function() {
+
+        console.log('videos')
+    }
+    btnDocumentos.onclick = function() {
+
+        console.log('documentos')
     }
 }
