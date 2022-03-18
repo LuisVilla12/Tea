19d0919cdd202c97439d0ab8820f2e3fa6aa672f
Luis Villa <luisji2312ia@gmail.com>
2022-03-17 19:01:41 -0600

    v31 Division infantes

--- build/app.js
+++ build/app.js
@@ -1,24 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     MenuDesplegable();
-    botones();
 });
-//FIXME: .hero era la clase
 const hero = document.querySelector('.menu__hidden');
 const body = document.querySelector('body');
-const btnPadre = document.querySelector('.eleccion_padre');
-const btnHijo = document.querySelector('.eleccion_hijo');
-
-function botones() {
-    btnPadre.onclick = function() {
-        document.location.href = "material_apoyo_padre.html";
-        console.log('click en btn padre')
-    }
-    btnHijo.onclick = function() {
-        document.location.href = "material_apoyo_infante.html";
-        console.log('click en btn hijo')
-    }
-}
-
 
 function MenuDesplegable() {
     const menu = document.querySelector('.menu');
@@ -31,22 +15,20 @@ function MenuDesplegable() {
         }
 
         divMenuDeslizable.classList.add('contenedor-menu');
-        const opcion = document.createElement('A');
+        const opcion0 = document.createElement('A');
         const opcion1 = document.createElement('A');
         const opcion2 = document.createElement('A');
         const opcion3 = document.createElement('A');
 
-        opcion.href = "index.html";
-        opcion1.href = "conocenos.html";
-        opcion2.href = "eleccion__material.html";
+        opcion1.href = "index.html";
+        opcion2.href = "conocenos.html";
+        opcion2.href = "eleccion__material";
         opcion3.href = "Registrarse.html";
 
-        opcion.innerHTML = '<i class="fa-solid fa-house"></i>Inicio';
         opcion1.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>Conocenos';
         opcion2.innerHTML = '<i class="fa-solid fa-book-open"></i>Material de apoyo';
         opcion3.innerHTML = '<i class="fa-solid fa-user"></i>Registrarse';
 
-        divMenuDeslizable.appendChild(opcion);
         divMenuDeslizable.appendChild(opcion1);
         divMenuDeslizable.appendChild(opcion2);
         divMenuDeslizable.appendChild(opcion3);
