92b5c1899af35b4ab3b6a992770035ec8ccb2608
Luis Villa <luisji2312ia@gmail.com>
2022-05-25 23:45:19 -0500

    conocenos

--- views/pages/index.php
+++ views/pages/index.php
@@ -11,102 +11,31 @@
             </div>
         </div>
     </section>
-    <section class="definicion contenedor">
-        <div class="definicion__contenido">
-            <h2>¿Qué es el autismo?</h2>
-            <p>Es un trastorno del desarrollo que afecta a la comunicación y a la interacción social, caracterizado por patrones de comportamiento restringidos, repetitivos y estereotipados.</p>
-        </div>
-        <img src="build/img/img4.jpg" alt="definicion" class="momentanea">
-    </section>
-    <section class="noticias">
-        <h2 class="noticias__titulo">Noticias...</h2>
-        <div class="contenedor">
-            <div class="noticias__grid">
-                <div class="noticia">
-                    <div class="noticia__titulo">
-                        <a href="/">
-                            <h4>Una mirada diferente sobre el autismo</h4>
-                        </a>
-                    </div>
-                    <img src="build/img/n1.jpg" alt="noticia__img">
-                    <div class="noticia__meta">
-                        <p><i class="fa-solid fa-calendar"></i>24-02-2022</p>
-                        <p><i class="fa-solid fa-clock"></i>19:00</p>
-                    </div>
-                </div>
-                <div class="noticia">
-                    <div class="noticia__titulo">
-                        <a href="/">
-                            <h4>Aumenta el riesgo de mortalidad</h4>
-                        </a>
-                    </div>
-                    <img src="build/img/n2.jpg" alt="noticia__img">
-                    <div class="noticia__meta">
-                        <p><i class="fa-solid fa-calendar"></i>24-02-2022</p>
-                        <p><i class="fa-solid fa-clock"></i>19:00</p>
-                    </div>
-                </div>
-                <div class="noticia">
-                    <div class="noticia__titulo">
-                        <a href="/">
-                            <h4>La placenta puede dar pistas sobre si tu hijo sufrirá autismo</h4>
-                        </a>
-                    </div>
-                    <img src="build/img/img1.jpg" alt="noticia__img">
-                    <div class="noticia__meta">
-                        <p><i class="fa-solid fa-calendar"></i>24-02-2022</p>
-                        <p><i class="fa-solid fa-clock"></i>19:00</p>
-                    </div>
+    <section class="conoce contenedor">
+        <div class="conoce__grid">
+            <div class="conoce__mask">
+                <img src="" alt="mask">
+            </div>
+            <div class="conoce__titulo">
+                <h3>Autismo lo que necesitas conocer.</h3>
+            </div>
+            <div class="conoce__descripcion">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam debitis dolorum suscipit quasi nisi officiis nobis exercitationem voluptatum culpa laboriosam.</p>
+                <div class="centrar">
+                    <a href="" class="conoce__boton">Conoce más</a>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="novedades">
-        <h2 class="novedades__titulo">Sabías que...</h2>
-        <div class="novedades__grid contenedor">
-            <div class="novedad">
-                <div class="novedad__mask">
-                    <img src="build/img/img1.jpg" alt="" class="novedad__img">
-                </div>
-                <div class="novedad__titulo">
-                    <h3>¿Cuáles son las señales?</h3>
-                </div>
-                <div class="novedad__contenido">
-                    <p>Algunas de las señales que puede presentar un infante con este trastorno pueden ser desde realizar movimientos repetitivos, como balancearse, girar o aletear con las manos hasta realizar actividades que podrían causarle daño, como
-                        morderse o golpearse la cabeza...
-                        <a href="https://www.mayoclinic.org/es-es/diseases-conditions/autism-spectrum-disorder/symptoms-causes/syc-20352928" class="novedad__enlace" target="_blank">Leer más...</a>
-                    </p>
-                </div>
+    <section class="sabias contenedor">
+        <div class="sabias__grid">
+            <div class="sabias__info">
+                <h3 class="sabias__titulo">Sabiás que</h3>
             </div>
-            <div class="novedad">
-                <div class="novedad__mask">
-                    <img src="build/img/img2.jpg" alt="" class="novedad__img">
-                </div>
-                <div class="novedad__titulo">
-                    <h3>Tecnología en la TEA</h3>
-                </div>
-                <div class="novedad__contenido">
-                    <p>Se conoce con el nombre de tecnología asistente, aquella dirigida a que las personas afectadas con autismo, TEA, etc; puedan comunicarse con el resto, así como darles herramientas para aprender habilidades sociales, identificar sujetos,
-                        situaciones, emociones, etc.
-                        <a href="https://blogs.uoc.edu/epce/es/potencial-nuevas-tecnologias-tic-intervencion-tea-autismo/" class="novedad__enlace" target="_blank">Leer más...</a>
-                    </p>
-                </div>
-            </div>
-            <div class="novedad">
-                <div class="novedad__mask">
-                    <img src="build/img/img3.jpg" alt="" class="novedad__img">
-                </div>
-                <div class="novedad__titulo">
-                    <h3>Interacciones sociales</h3>
-                </div>
-                <div class="novedad__contenido">
-                    <p>Algunos de los problemas que puede presentar un infante con este trastorno en cuanto a relacionar de manera social pueden ser que no responde a su nombre o, en ocasiones, parece no escuchar, además de resistirse a los abrazos y las
-                        caricias; además, parece que prefiere jugar solo y se abstrae en su propio mundo...
-                        <a href="https://www.mayoclinic.org/es-es/diseases-conditions/autism-spectrum-disorder/symptoms-causes/syc-20352928" class="novedad__enlace" target="_blank">Leer más...</a>
-                    </p>
-                </div>
-            </div>
+        </div>
+    </section>   
+    
             <!-- <div class="novedad">
                 <div class="novedad__mask">
                     <img src="build/img/img3.jpg" alt="" class="novedad__img">
@@ -120,5 +49,4 @@
                     </p>
                 </div>
             </div> -->
-        </div>
-    </section>
+    
